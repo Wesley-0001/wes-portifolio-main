266548fdad8849af41c706fc63ce5ee8903ed928
wes <wesley2014.wv53@gmail.com>
2026-04-17 12:22:49 -0300

    Updates

--- assets/Portfolio3D-CaRHf858.js
+++ assets/Portfolio3D-CaRHf858.js
@@ -1,4 +1,4 @@
-import{r as lo,j as ho}from"./index-D6YO6Mrq.js";/**
+import{r as lo,j as ho}from"./index-BDKuG2lv.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
