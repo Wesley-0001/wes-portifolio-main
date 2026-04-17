fca1524aa73f700c7cd1257c44b07233bb508d2d
wes <wesley2014.wv53@gmail.com>
2026-04-17 14:41:12 -0300

    Updates

--- assets/Portfolio3D-sgVx9_hl.js
+++ assets/Portfolio3D-sgVx9_hl.js
@@ -1,4 +1,4 @@
-import{r as lo,j as ho}from"./index-CHwXCoyV.js";/**
+import{r as lo,j as ho}from"./index-DQboJPF-.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
